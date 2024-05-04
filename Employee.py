3999a0cf10f2c110a144f5864b424f8fb2c55b7c
NataliaKowal <nataliakowal328@gmail.com>
2024-05-04 18:14:04 +0200

    Work in prorgess

--- Employee.py
+++ Employee.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Employee:
     def __init__(self):
         self.Id = 0 
@@ -18,3 +20,64 @@ class Employee:
         self.Departemnt_Id = new_Departemnt_Id
         self.Room_number = new_Room_number
         self.Salary = new_Salary
+    
+def list_employees (DB_path):
+    try: 
+        with sqlite3.connect(DB_path) as con:
+            cur = con.cursor()
+            records = cur.execute("SELECT * FROM Employees").fetchall()
+            employees = []
+            print("Employees in database:")
+            for record in records:
+                employee = Employee(record)
+                print(f" - {employee}")
+                employees.append(employee)
+                
+    except sqlite3.Error as e: 
+        print(f"error: {e}")
+
+
+def add_employee (DB_path, Name, Surname, PESEL_number, Degree_Id, Departemnt_Id, Room_number, Salary):
+    """
+    Opis: Fucnkj.......
+    
+    Autor: Imię i nazwisko autora
+    
+    Argumenty:
+    DB_path (str): Opis argumentu arg1.
+    Name (str): Opis argumentu arg2.
+    """
+    try: 
+        with sqlite3.connect(DB_path) as con:
+            cur = con.cursor()
+            querry = """
+                INSERT INTO Employees (Name, Surname, PESEL_number, Degree_Id, Departemnt_Id, Room_number, Salary) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)"""
+            cur.execute(querry, (Name, Surname, PESEL_number, Degree_Id, Departemnt_Id, Room_number, Salary ))
+            con.commit()
+            print(f"Added new employee to database with data: Name: {Name}, Surname: {Surname}, PESEL_number: {PESEL_number}, Degree_Id: {Degree_Id}, Departemnt_Id: {Departemnt_Id}, Room_number: {Room_number}, Salary: {Salary}")
+    except sqlite3.Error as e: 
+        print(f"error: {e}")
+
+def edit_employee (DB_path, Id, Name, Surname, PESEL_number, Degree_Id, Departemnt_Id, Room_number, Salary):
+    try: 
+        with sqlite3.connect(DB_path) as con:
+            cur = con.cursor()
+            querry = """
+                UPDATE Employees 
+                SET Name = ?, Surname = ?, PESEL_number = ?, Degree_Id = ?, Departemnt_Id = ?, Room_number = ?, Salary = ?  
+                WHERE Id = ?"""
+            cur.execute(querry, (Name, Surname, PESEL_number, Degree_Id, Departemnt_Id, Room_number, Salary, Id))
+            con.commit()
+    except sqlite3.Error as e: 
+        print(f"error: {e}")
+
+def delete_employee (DB_path, Id):
+    try: 
+        with sqlite3.connect(DB_path) as con:
+            querry = "DELETE FROM Employees WHERE Id = ?"
+            cur = con.cursor()
+            cur.execute(querry, (Id, ))
+            con.commit()
+    except sqlite3.Error as e: 
+        print(f"error: {e}")
