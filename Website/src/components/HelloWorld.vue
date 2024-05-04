552bba32c77e55bf7eba918cbd61f075969bb9ad
NataliaKowal <nataliakowal328@gmail.com>
2024-05-04 18:54:14 +0200

    added preparations for Website

---
+++ Website/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Witaj na mojej stronie Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+/* Tutaj możesz dodać style CSS */
+</style>
