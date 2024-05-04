88f340de32b722033821db18aee49fb4340f8bfa
NataliaKowal <nataliakowal328@gmail.com>
2024-05-05 00:04:08 +0200

    some work with cpnnection to backend server

--- Backend/server.py
+++ Backend/server.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from DatabaseInterface.Degree import *
 
 app = Flask(__name__, static_folder='.', template_folder='.')
 
+CORS(app)
+
 @app.route("/hello_world")
 def hello_world():
-    return "Hello World!"
+    return jsonify(message="Ziemniaczki z koperkiem!")
 
+
+@app.route("/get_all_degrees")
+def get_all_degrees():
+    return list_degrees()
 
 if __name__ == '__main__':
+    # Zmiana portu na 12345
     app.run(host="0.0.0.0", port=1234)
--- Website/src/views/EmployeesView.vue
+++ Website/src/views/EmployeesView.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
     <h1>Widok na pracowników</h1>
+    <!-- Wyświetlanie odpowiedzi serwera pod nagłówkiem -->
+    <h1>{{ responseMessage }}</h1>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      // Zmienna do przechowywania odpowiedzi serwera
+      responseMessage: ''
+    }
+  },
+  mounted() {
+    // Wysyłanie zapytania do serwera po załadowaniu komponentu
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios.get('http://localhost:1234/hello_world')
+        .then(response => {
+          // Przechowywanie odpowiedzi serwera, dostosowanie do struktury JSON
+          this.responseMessage = response.data.message;
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+        });
+    }
+  }
+}
 </script>
 
 <style>
-
+/* Tutaj możesz dodać style CSS, jeśli potrzebujesz */
 </style>
